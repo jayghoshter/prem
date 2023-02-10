538fc026233d957335987aef538afe45b3377844
Jayghosh Rao <jayghoshter@gmail.com>
2023-02-10 11:42:08 +0100

    implement pikepdf support

--- ptdpy/PDF.py
+++ ptdpy/PDF.py
@@ -1,28 +1,50 @@
 from pdfrw import PdfReader, PdfWriter, IndirectPdfDict, PdfDict
 import pdfplumber
 import os
+from pikepdf import Pdf, Name
 
 class PDF:
     metadata: dict
 
-    def __init__(self, filename=None):
+    def __init__(self, filename=None, engine='pikepdf'):
         self.filename = filename
         self.metadata = {}
         self.pages = None
+        self.engine = engine
+
+        if self.engine == 'pikepdf':
+          self.load = self._load_pikepdf
+          self.write = self._write_pikepdf
+        elif self.engine == 'pdfrw':
+          self.load = self._load_pdfrw
+          self.write = self._write_pdfrw
+        else:
+          raise ValueError(self.engine)
+
 
         self.load()
 
-    def load(self, filename=None):
+    def _load_pdfrw(self, filename=None):
         filename = filename if filename else self.filename
 
         if not filename:
             return
 
         reader = PdfReader(filename)
-        self.metadata = self._pdfdict_to_dict(reader.Info)
+        self.metadata = self._pdfrwdict_to_dict(reader.Info)
         self.pages = reader.pages
 
-    def write(self, filename=None):
+    def _load_pikepdf(self, filename=None):
+        filename = filename if filename else self.filename
+
+        if not filename:
+            return
+
+        with Pdf.open(filename) as pdf:
+            self.metadata = {f"{str(k)[1:]}": str(v) for k,v in pdf.docinfo.items()}
+            self.pages = pdf.pages
+
+    def _write_pdfrw(self, filename=None):
         filename = filename if filename else self.filename
 
         writer = PdfWriter()
@@ -30,12 +52,21 @@ class PDF:
         writer.trailer.Info = IndirectPdfDict(**self.metadata)
         writer.write(filename)
 
+    def _write_pikepdf(self, filename=None):
+        with Pdf.open(self.filename, allow_overwriting_input=True) as pdf:
+            for k,v in self.metadata.items(): 
+                key = f"/{k}"
+                pdf.docinfo[key] = v
+            with pdf.open_metadata() as meta:
+                meta.load_from_docinfo(pdf.docinfo)
+            pdf.save(filename)
+
     def rename(self, newname):
         self.write(newname)
         os.remove(self.filename)
         self.filename = newname
 
-    def _pdfdict_to_dict(self, data:PdfDict):
+    def _pdfrwdict_to_dict(self, data:PdfDict):
         return { k[1:] : str(v)[1:-1].replace('\\', '') for k,v in data.items() }
 
     def unload(self):
