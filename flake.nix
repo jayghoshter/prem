34ba5d79bff327aa4c82197edde81d1c4d7f22a1
Jayghosh Rao <jayghoshter@gmail.com>
2023-02-17 18:41:58 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,58 +1,55 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  description = "A Python dev shell for a given package.";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils = {
+      url = github:numtide/flake-utils;
+    };
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db";
+      flake = false;
+      # inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.mach-nix.follows = "mach-nix";
+      # follows = "nixpkgs";
+    };
+    mach-nix = {
+      url = "mach-nix";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+      inputs.flake-utils.follows = "flake-utils";
+      # inputs.nixpkgs.follows = "nixpkgs"; # causes issues with setuptools/distlib
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let 
-    pkgs = nixpkgs.legacyPackages.${system}; 
-    # dir = ''/home/jayghoshter/dev/tools/ptdpy/'';
-
-    mach-nix = import (builtins.fetchGit {
-      url = "https://github.com/DavHau/mach-nix";
-      ref = "refs/tags/3.5.0";
-    }) {};
-
-
-    in 
-    {
-
-      defaultPackage = pkgs.python3Packages.buildPythonPackage{
-        pname = "ptdpy";
-        version = "0.1";
-
-        src = ./.;
-
-        mnpyreq = mach-nix.mkPython {
+  outputs = { self, nixpkgs, flake-utils, mach-nix, pypi-deps-db, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        machNix = mach-nix.lib."${system}";
+        devEnvironment = machNix.mkPython {
+            python="python39";
           requirements = builtins.readFile ./requirements.txt;
+            # Fix some broken dependencies for python310
+          _.rich.propagatedBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [ pySelf.poetry ];
+          _.jeepney.propagatedBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [ pySelf.outcome pySelf.trio ];
         };
-
-        propagatedBuildInputs = with pkgs; [
-          mnpyreq
-        ];
-
-        doCheck = false;
-      };
-
-
-      devShell = pkgs.mkShell rec {
+      in
+      {
+        devShell = pkgs.mkShell {
             name = "ptdpy";
-
-            mnpyreq = mach-nix.mkPython {
-              requirements = builtins.readFile ./requirements.txt;
-            };
-
-            buildInputs = with pkgs; [
-              mnpyreq
-            ];
-
+          buildInputs = [
+            devEnvironment
+          ];
             shellHook = ''
               # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
               # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+              export PIP_PREFIX=$(pwd)/_build/pip_packages
+              export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
               export PYTHONPATH="$(pwd):$PYTHONPATH"
-              export PATH="$(pwd)/bin:$PATH"
+              export PATH="$(pwd):$PATH"
+              export PATH="$PIP_PREFIX/bin:$PATH"
               unset SOURCE_DATE_EPOCH
             '';
-
         };
+        packages.venv = devEnvironment;
       });
 }
